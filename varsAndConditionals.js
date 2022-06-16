02653e46d0ddf172e7b9e2ea40cd7bd5f9e78e72
Zachary Cooper <zacharycooper@Zacharys-Air.localdomain>
2022-06-16 12:53:06 -0600

    finished kenobi

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -3,3 +3,32 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+let backpack = []
+
+backpack.push("Lightsaber")
+backpack.push("jedi robe", "food")
+    console.log(backpack)
+
+ let belt = backpack.shift()
+ 
+ backpack.splice(1, 0, "coat")
+  
+ let bag = []
+
+ bag.unshift("flint", "blanket", "toothbrush")
+ 
+ backpack.push(bag.slice(0, 3))
+
+//console.log(bag)
+//console.log(backpack.length)
+//console.log(belt)
+ 
+for(let i = 0; i < bag.length; i++) {
+    console.log(bag[i])
+}
+
+for(let i = 0; i < 3; i++) {
+    console.log(backpack[i]) 
+}
+    
